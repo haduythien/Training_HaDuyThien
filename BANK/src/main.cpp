7aaacf5faaa31c65c1e62a90e5bae853d34d3e10
haduythien <haduythien1997@gmail.com>
2021-12-24 09:17:52 +0700

    create 3 project training: BAKERY, BANK, CHAT_ROOM

---
+++ BANK/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "create main.cpp Bank\n";
+
+	return 0;
+}
---
+++ CHATROOM/src/server.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "create server.cpp Chatroom";
+
+	return 0;
+}
