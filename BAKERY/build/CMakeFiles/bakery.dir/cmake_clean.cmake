7f5480e50680dbd339965bdd7b2907330f4e9a34
haduythien <haduythien1997@gmail.com>
2021-12-24 09:57:09 +0700

    add CMakeList.txt: Bakery&bank

---
+++ BAKERY/build/CMakeFiles/bakery.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bakery.dir/src/main.cpp.o"
+  "bakery.pdb"
+  "bakery"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bakery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ BANK/build/CMakeFiles/bank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bank.dir/src/main.cpp.o"
+  "bank.pdb"
+  "bank"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
